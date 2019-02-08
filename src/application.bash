9266118a70a77be73e65c38ed5d9c0752624eaed
Corentin Lancien <c.lancien@iia-laval.fr>
2019-02-08 11:07:51 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,5 @@
 ### Application.bash - 2019 - Lancien ###
-echo "Copyright Company - 2019"
-echo "Version 1.1"
-echo "Build : 10:00"
+echo "Version 2"
+"Build : 11:00"
+echo "Build ${build_hour}"
+echo "version ${version}"
